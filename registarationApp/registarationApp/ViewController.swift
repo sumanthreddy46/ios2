7ffacf38f201ba6e1a6e22e78e0f490a7eeba66a
sumanthreddy46 <39779013+sumanthreddy46@users.noreply.github.com>
2018-11-13 12:56:05 -0500

    Add files via upload

---
+++ registarationApp/registarationApp/ViewController.swift
@@ -0,0 +1,110 @@
+//
+//  ViewController.swift
+//  registarationApp
+//
+//  Created by MacStudent on 2018-11-13.
+//  Copyright © 2018 MacStudent. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    
+        btnRegister.isHidden = true
+        btnModify.isHidden = true
+        btnSave.isHidden = true
+        btnLogin.isHidden = true
+        lblEmail.isHidden = true
+        txtEmail.isHidden = true
+        lblEncryption.isHidden = true
+        switchEnc.isHidden = true
+        lblFirstname.isHidden = true
+        txtFirstname.isHidden = true
+        lblLastname.isHidden = true
+        txtLastname.isHidden = true
+        btnModify.isHidden = true
+        btnSave.isHidden = true
+        btnLogin.isHidden = true
+        lblUsername.isHidden = true
+        lblPassword.isHidden = true
+        txtUsername.isHidden = true
+        txtPassword.isHidden = true
+        
+        
+    }
+
+    @IBOutlet weak var switchEnc: UISwitch!
+    @IBOutlet weak var lblEncryption: UILabel!
+    @IBOutlet weak var txtEmail: UITextField!
+    @IBOutlet weak var lblEmail: UILabel!
+    @IBOutlet weak var txtLastname: UITextField!
+    @IBOutlet weak var lblLastname: UILabel!
+    @IBOutlet weak var txtFirstname: UITextField!
+    @IBOutlet weak var lblFirstname: UILabel!
+    @IBOutlet weak var txtPassword: UITextField!
+    @IBOutlet weak var lblPassword: UILabel!
+    @IBOutlet weak var lblUsername: UILabel!
+    @IBOutlet weak var txtUsername: UITextField!
+    @IBOutlet weak var btnRegister: UIButton!
+    
+    @IBOutlet weak var btnLogin: UIButton!
+    @IBOutlet weak var btnSave: UIButton!
+    @IBOutlet weak var btnModify: UIButton!
+    @IBOutlet weak var userState: UISegmentedControl!
+    
+   
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+   
+    
+    @IBAction func userstate(_ sender: UISegmentedControl) {
+        if( sender.selectedSegmentIndex == 0){
+            btnModify.isHidden = true
+            btnSave.isHidden = true
+            btnLogin.isHidden = true
+            btnRegister.isHidden = false
+            lblEmail.isHidden = false
+            txtEmail.isHidden = false
+            lblEncryption.isHidden = false
+            switchEnc.isHidden = false
+            lblFirstname.isHidden = false
+            txtFirstname.isHidden = false
+            lblLastname.isHidden = false
+            txtLastname.isHidden = false
+            lblUsername.isHidden = false
+            lblPassword.isHidden = false
+            txtUsername.isHidden = false
+            txtPassword.isHidden = false
+            
+        }else{
+            lblUsername.isHidden = false
+            lblPassword.isHidden = false
+            txtUsername.isHidden = false
+            txtPassword.isHidden = false
+            btnRegister.isHidden = true
+            lblEmail.isHidden = true
+            txtEmail.isHidden = true
+            lblEncryption.isHidden = true
+            switchEnc.isHidden = true
+            lblFirstname.isHidden = true
+            txtFirstname.isHidden = true
+            lblLastname.isHidden = true
+            txtLastname.isHidden = true
+            btnModify.isHidden = false
+            btnSave.isHidden = false
+            btnLogin.isHidden = false
+            
+        }
+    }
+    @IBAction func acRegister(_ sender: Any) {
+    }
+    
+}
+
